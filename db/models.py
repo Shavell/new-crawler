e3c3651df2129ffa9d032df69adb000b9005745d
Umit Yarimbas <admin@shavell.org>
2020-10-18 20:34:53 +0300

    reformat models.py

--- db/models.py
+++ db/models.py
@@ -8,40 +8,40 @@ db = SqliteDatabase(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath
 
 
 class StoreId(Model):
-    id = PrimaryKeyField()
-    storeUrl = CharField(unique=True)
-    comment = CharField()
-    created = DateTimeField(default=datetime.now)
-    done = BooleanField(default=False)
+    id        = PrimaryKeyField()
+    storeUrl  = CharField(unique=True)
+    comment   = CharField()
+    created   = DateTimeField(default=datetime.now)
+    done      = BooleanField(default=False)
 
     class Meta:
         database = db
         table_name = 'store_id'
 
 class Store(Model):
-    id = PrimaryKeyField()
-    storeId = ForeignKeyField(StoreId, backref='stores', field='id', index=True)
-    storeName = CharField(null=True)
-    products = IntegerField(null=True)
-    following = CharField(null=True)
-    chatPerformance = CharField(null=True)
-    cancellationRate = CharField(null=True)
-    joined = CharField(null=True)
-    followers = CharField(null=True)
-    rating = CharField(null=True)
-    about = CharField(null=True, max_length=2500)
+    id                = PrimaryKeyField()
+    storeId           = ForeignKeyField(StoreId, backref='storeid', field='id')
+    storeName         = CharField(null=True, max_length=250)
+    products          = CharField(null=True, max_length=250)
+    following         = CharField(null=True, max_length=250)
+    chatPerformance   = CharField(null=True, max_length=250)
+    cancellationRate  = CharField(null=True, max_length=250)
+    joined            = CharField(null=True, max_length=250)
+    followers         = CharField(null=True, max_length=250)
+    rating            = CharField(null=True, max_length=250)
+    about             = CharField(null=True, max_length=2500)
 
     class Meta:
         database = db
         table_name = 'store'
 
 class StoreItem(Model):
-    id = PrimaryKeyField()
-    store = ForeignKeyField(Store, backref='id', field='id', index=True)
-    productName = CharField(null=True)
-    productPrice = CharField(null=True)
-    productUrl = CharField(null=True)
-    productSold = CharField(null=True)
+    id            = PrimaryKeyField()
+    store         = ForeignKeyField(Store, backref='store', field='id')
+    productName   = CharField(null=True)
+    productPrice  = CharField(null=True)
+    productUrl    = CharField(null=True)
+    productSold   = CharField(null=True)
 
     class Meta:
         database = db
