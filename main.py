e327479321906c5044db267de3db597e3eb97e09
Umit Yarimbas <admin@shavell.org>
2020-10-18 19:23:20 +0300

    main.py updated

--- main.py
+++ main.py
@@ -17,47 +17,29 @@ class Main(unittest.TestCase):
                 options.add_argument('headless')
             self.driver = webdriver.Chrome(executable_path=Constants.chrome_driver, chrome_options=options)
         models.db.connect()
-        self.test_id = fill_start_test_query(start=datetime.now())
 
     def tearDown(self):
         self.driver.quit()
         models.db.close()
 
-    # Triggered automatically
-    def check_tracking(self, err=False):
+    def test_doit(self):
         driver = self.driver
-        driver.get(Constants.url)
-        tracking_codes = models.TrackingCode.select().where(models.TrackingCode.active == True)
+        store_ids = models.StoreId.select().where(models.StoreId.done == False)
 
-        for code in tracking_codes:
-            with self.subTest(code=code.tracking_id):
-                barcode = code.tracking_id
-                start = datetime.now()
-                extracted_captcha_key = default_scenario(self, barcode)
-
+        for store_id in store_ids:
+            with self.subTest(comment=store_id.comment):
+                driver.get(store_id.storeUrl)
                 try:
-                    check_success_response(self)
-                except CaptchaErrorException as error:
-                    logger.error("Captcha is wrong! tracking_code: {}".format(barcode))
-                    fill_test_action_to_db(err, barcode, extracted_captcha_key, start, error, self.test_id)
-                    default_scenario(self, barcode)
+                    crawl_store(self)
                 except ScreenWaitException:
                     time.sleep(Constants.wait_for_sec)
-                    logger.info("Screen is waiting! 5 sec. after call refresh ")
+                    logger.info("Page is loading! 5 sec. for refresh")
                     self.driver.refresh()
-                except BarcodeErrorException as error:
-                    logger.error("Barcode is wrong !")
-                    err = True
-                    fill_test_action_to_db(err, barcode, extracted_captcha_key, start, error, self.test_id)
+                except InvalidStoreUrlException as error:
+                    logger.error("StoreUrl is wrong !")
                     self.assertFalse(error)
                 except Exception as error:
-                    err = True
-                    fill_test_action_to_db(err, barcode, extracted_captcha_key, start, error, self.test_id)
                     self.assertFalse(error)
-
-                action_id = fill_test_action_to_db(err, barcode, extracted_captcha_key, start, "", self.test_id)
-                fill_test_result_to_db(self, action_id)
-
 
 if __name__ == "__main__":
     unittest.main()
